dc9c1aab705f9348dbab39cd4e91495a28f55709
alvzed <alvin.zedendahl@gmail.com>
2020-06-01 16:08:26 +0000

    changed color and started on the bricks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,74 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
 var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
-var paddleHeight = 10;
-var paddleWidth = 75;
+var paddleHeight = 15;
+var paddleWidth = 100;
 var paddleX = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
+//Bricks 
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 }
+    }
+}
+
+function drawBricks() {
+    for(var c = 0; c < brickColumnCount; c++) {
+        for(var r = 0; r < brickRowCount; r++) {
+            bricks[c][r].x = 0;
+            bricks[c][r].y = 0;
+            ctx.beginPath();
+            ctx.rect(0, 0, brickWidth, brickHeight);
+            ctx.fillStyle = "#a86878";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false); 
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true; 
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false; 
+    } else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
 
 //Drawing the ball (shape and color)
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#a06878";
     ctx.fill();
     ctx.closePath();
 }
@@ -22,7 +76,7 @@ function drawBall() {
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#a06878";
     ctx.fill();
     ctx.closePath();
 }
@@ -31,18 +85,39 @@ function drawPaddle() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     
     //following ifs make the ball bounce off the walls 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
 
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            alert("game over");
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
+
+    if(rightPressed) {
+        paddleX += 7;
+        if(paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    } else if(leftPressed) {
+        paddleX -= 7;
+        if(paddleX < 0) {
+            paddleX = 0;
+        }
     }
 
     x += dx;
     y += dy;
 }
 
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
