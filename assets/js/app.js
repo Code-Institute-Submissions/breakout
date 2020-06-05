0b94de5c6c5e15860aad6e06af0705a4958fc5ea
alvzed <alvin.zedendahl@gmail.com>
2020-06-05 12:01:43 +0000

    added the paddle

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,24 @@ const paddleWidth = 100;
 const paddleHeight = 15;
 const paddleMarginBottom = 30;
 
+const paddle = {
+    xPosition: canvas.width/2 - paddleWidth/2,
+    yPosition: canvas.height - paddleHeight - paddleMarginBottom,
+    width: paddleWidth,
+    height: paddleHeight,
+    dx: 6
+}
 
+function drawPaddle() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(paddle.xPosition, paddle.yPosition, paddle.width, paddle.height);
+}
+
+function draw();
 
 function game() {
 
+    drawPaddle();
     draw();
     update();
     requestAnimationFrame(game);
