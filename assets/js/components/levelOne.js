b3e2fd1a54e6f785cc31b0fe5f835c520152dc13
alvzed <alvin.zedendahl@gmail.com>
2020-06-10 14:44:43 +0000

    split the ball and brick objects to different files to make changing the levels easier

---
+++ assets/js/components/levelOne.js
@@ -0,0 +1,26 @@
+// ------- Levels ---------//
+let level = 1;
+let maxLevel = 1;
+// ----- Bricks ----- //
+var brick = {
+    row: 3,
+    column: 3,
+    width: 260,
+    height: 15,
+    offsetLeft: 45,
+    offsetTop: 20,
+    marginTop: 40,
+    borderColor: "white",
+    fillColor: "white"
+}
+
+// ----------- The ball ------------- //
+const ballRadius = 10;
+var ball = {
+    xPosition: 1, //The 1's  are here simply to be overwritten
+    yPosition: 1 - ballRadius,
+    radius: ballRadius,
+    speed: 6,
+    dx: 5,
+    dy: -5
+}
