bce2c03ffcf3c0e4c90d18dcf60bab9f3e3ec5f2
alvzed <alvin.zedendahl@gmail.com>
2020-06-10 14:57:49 +0000

    added level twos variables and added a 'next level' button on win screen

---
+++ assets/js/components/levelTwo.js
@@ -0,0 +1,26 @@
+// ------- Levels ---------//
+let level = 2;
+let maxLevel = 2;
+// ----- Bricks ----- //
+var brick = {
+    row: 3,
+    column: 5,
+    width: 160,
+    height: 15,
+    offsetLeft: 25,
+    offsetTop: 20,
+    marginTop: 40,
+    borderColor: "white",
+    fillColor: "white"
+}
+
+// ----------- The ball ------------- //
+const ballRadius = 10;
+var ball = {
+    xPosition: 1, //The 1's  are here simply to be overwritten
+    yPosition: 1 - ballRadius,
+    radius: ballRadius,
+    speed: 6,
+    dx: 5,
+    dy: -5
+}
